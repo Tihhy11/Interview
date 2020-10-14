049002f7b3905b322f76197a1324e2f2c3fa0f91
Tihhy11 <dtihhy@gmail.com>
2020-10-14 11:41:10 +0300

    Add files via upload

---
+++ email.php
@@ -0,0 +1,9 @@
+<center>
+Привет, <?php echo htmlspecialchars ($_POST ['name']);?>.
+<BR>
+Спасибо за ваш интерес.
+<BR>
+Вас интересуют <?php echo htmlspecialchars ($_POST ['preference']);?>.
+<BR>
+Информацию о них мы пошлем вам на email: <?php echo htmlspecialchars ($_POST ['email']);?>.
+</CENTER>
