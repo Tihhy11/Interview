b18cea2b2758c1b9db19b6c38bacd65d6e23c32c
Tihhy11 <dtihhy@gmail.com>
2020-10-14 11:14:52 +0300

    Add files via upload

---
+++ sql.php
@@ -0,0 +1,23 @@
+﻿<?php
+//phpinfo();
+$serverName = "BEST-KOMP\\sqlexpress"; 
+$connectionInfo = array( "Database"=>"Interview", "UID"=>"Tihhy", "PWD"=>"454215718", "CharacterSet"  => 'UTF-8');
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+if( $conn ) {
+     echo "Connection established.<br />";
+}else{
+     echo "Connection could not be established.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
+
+
+$sql = 'insert into [Users] (lastname, firstname, age) values (?, ?, ?)';
+$params = array ('Победа', 'победа вместо обеда', 4221);
+$result = sqlsrv_query ($conn , $sql, $params);
+
+
+if( $result === false ) {
+     die( print_r( sqlsrv_errors(), true));
+}
+?>
